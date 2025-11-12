a38fd1d8209f31a7473996b3cc2a54c838d3dfd5
Karl George <karlgeorge87@gmail.com>
2025-11-12 11:26:01 +0100

    Add hero image

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,25 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <main className='container'></main>;
+  return (
+    <main>
+      <section className='relative h-[600px] lg:h-screen'>
+        <Image
+          src={'/images/hero.png'}
+          alt='Hero'
+          fill
+          sizes='100vw'
+          className='object-cover'
+        />
+        <div className='flex flex-col absolute top-1/7 left-1/2 -translate-x-1/2 lg:w-[1050px] text-center gap-5'>
+          <h1 className='hero-title'>
+            Make Your Interior More Minimalistic & Modern
+          </h1>
+          <p className='text-white text-2xl'>
+            Turn your room into a more minimalist and modern living area
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
