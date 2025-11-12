2719f6c908f530308bf167e819567c907129a95b
Karl George <karlgeorge87@gmail.com>
2025-11-12 13:54:03 +0100

    Style hero text for responsive design

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-      <section className='relative h-[600px] lg:h-screen'>
+      <section className='relative h-[900px] lg:h-screen'>
         <Image
           src={'/images/hero.png'}
           alt='Hero'
@@ -11,11 +11,11 @@ export default function Home() {
           sizes='100vw'
           className='object-cover'
         />
-        <div className='flex flex-col absolute top-1/7 left-1/2 -translate-x-1/2 lg:w-[1050px] text-center gap-5'>
+        <div className='flex flex-col absolute top-1/6 lg:top-1/7 left-1/2 -translate-x-1/2 lg:w-[1050px] w-[80%] text-center gap-5'>
           <h1 className='hero-title'>
             Make Your Interior More Minimalistic & Modern
           </h1>
-          <p className='text-white text-2xl'>
+          <p className='text-white text-lg lg:text-2xl'>
             Turn your room into a more minimalist and modern living area
           </p>
         </div>
